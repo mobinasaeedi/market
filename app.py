6ae1a05dce8cb83b407877c153b11c6b4076b7e1
Mobina Saeedi <mobinasaeedi863@gmail.com>
2025-05-09 10:59:00 +0330

    admin and user added to blueprint

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from blueprint.general import app as general
+from blueprint.user import app as user
+from blueprint.admin import app as admin
+
 
 app=Flask(__name__)
 app.register_blueprint(general)
+app.register_blueprint(user)
+app.register_blueprint(admin)
 
 if __name__=="__main__":
     app.run()
